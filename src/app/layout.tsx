9c400ada0d2e6e74b52d5cd678d4b601e96bf1c0
Jacob Chademwiri <jchademwiri@gmail.com>
2025-07-18 10:05:47 +0200

    changed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
